5accc423be1f599c3fd4529addd53afaba62505a
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2023-05-24 18:06:22 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import { mongooseConnection } from "./database/mongo.js";
+import { statusLog } from "./utils/logs.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import pkg from "express";
+import * as dotenv from "dotenv";
+import { errorHandler } from "./utils/errorHandler.js";
+
+//routes
+import routes from "./routes/mainRoutes.js";
+
+
+mongooseConnection();
+dotenv.config();
+const app = pkg();
+
+//middlewares
+app.use(cors({ credentials: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use("", routes);
+app.use(errorHandler);
+app.listen(process.env.PORT || 3000, () => {
+  statusLog(`server is listening at ${process.env.PORT}`);
+});
