7577941e772c7e58aa388a190113152e4669271d
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2023-05-24 17:32:12 +0530

    login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,36 @@
+import * as EmailValidator from "email-validator";
+import bcrypt from "bcrypt";
+import { createJwtToken } from "../utils/jwtUtils.js";
+import { userModel } from "../schema/userModel.js";
+import { errorLog } from "../utils/logs.js";
+
+
+export const loginView = (req, res) => {
+  if (EmailValidator.validate(req.body.email)) {
+    userModel.findOne({ email: req.body.email }).then((queryResults) => {
+      if (!queryResults) {
+        res.status(404).send({ message: "Email not found" });
+      } else {
+        bcrypt
+          .compare(req.body.password, queryResults.password)
+          .then(function (result) {
+            if (result) {
+              const token = createJwtToken({
+                email: req.body.email,
+                isAdmin: queryResults.isAdmin,
+              });
+              res.status(200).send({ message: "success", authToken: token });
+            } else {
+              res.status(401).send({ message: "Password does not match" });
+            }
+          })
+          .catch((error) => {
+            errorLog(error);
+          });
+      }
+    });
+  } else {
+    res.status(400).send({ message: "Invalid Email" });
+  }
+};
 
