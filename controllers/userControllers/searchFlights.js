75c12695a9c303c9a4b7e5eb4a5253342e2c95df
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2023-05-26 13:57:03 +0530

    searchFlights.js

--- controllers/userControllers/searchFlights.js
+++ controllers/userControllers/searchFlights.js
@@ -1,7 +1,7 @@
 import { flightModel } from "../../schema/flightModel.js";
 export const searchFlights = (req, res) => {
   flightModel
-    .find({takeoff : req.date})
+    .find({takeoff : req.body.date})
     .then((queryResults) => {
       res.send(queryResults);
     })
